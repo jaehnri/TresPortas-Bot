e80699d9ab32242ff2fb16b3dcb8d8feaeea9267
jae hnri <joao.henri.cr@gmail.com>
2019-03-22 16:08:56 -0300

    comment

--- twitter_bot_3portas.py
+++ twitter_bot_3portas.py
@@ -14,17 +14,17 @@ api = tweepy.API(auth)
 
 FILE_NAME = 'last_seen_id.txt'
 
-def retrieve_last_seen_id(file_name):
-    f_read = open(file_name, 'r')
-    last_seen_id = int(f_read.read().strip())
-    f_read.close()
-    return last_seen_id
+#def retrieve_last_seen_id(file_name):
+ #   f_read = open(file_name, 'r')
+  #  last_seen_id = int(f_read.read().strip())
+   # f_read.close()
+    #return last_seen_id
 
-def store_last_seen_id(last_seen_id, file_name):
-    f_write = open(file_name, 'w')
-    f_write.write(str(last_seen_id))
-    f_write.close()
-    return
+#def store_last_seen_id(last_seen_id, file_name):
+ #   f_write = open(file_name, 'w')
+  #  f_write.write(str(last_seen_id))
+   # f_write.close()
+    #return
 
 def reply_to_tweets():
     print('retrieving and replying to tweets...', flush=True)
